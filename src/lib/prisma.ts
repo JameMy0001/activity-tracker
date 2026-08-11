e70cefe5f1316e5fd0ee0da1ef9e9e6ab368fc10
Developer <dev@example.com>
2026-08-11 18:26:36 +0700

    fix: explicit import path for generated Prisma Client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../generated/prisma/client'
+import { PrismaClient } from '../../generated/prisma/client/client'
 import { createClient } from '@libsql/client'
 import { PrismaLibSql } from '@prisma/adapter-libsql'
 
